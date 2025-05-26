d9662398370dd8c8557388c16262242f94d79621
hp <hp@example.com>
2025-05-25 22:38:01 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+    required_providers = ">= 1.2.0"
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+    ami = ""
+    instance_type = "t4g.small"
+    tags = {
+        Name = "exampleInstance"
+    }
+}
