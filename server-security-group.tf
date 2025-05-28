e269ab84979c420f3f6e626e5f3c591f3fcdf4ea
hp <hp@example.com>
2025-05-27 22:21:35 -0400

    fix bugs

--- server-security-group.tf
+++ server-security-group.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "server" {
 resource "aws_vpc_security_group_egress_rule" "allow_server_egress_ipv4" {
     security_group_id = aws_security_group.server.id
     cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = "-1"
+    ip_protocol = -1
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_server_egress_ipv6" {
     security_group_id = aws_security_group.server.id
     cidr_ipv6 = "::/0"
-    ip_protocol = "-1"
+    ip_protocol = -1
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_server_tcp_8080_from_subnet" {
--- webserver-security-group.tf
+++ webserver-security-group.tf
@@ -13,21 +13,21 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_egress_ipv6" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv6 = "::/0"
-    ip_protocol = "-1"
+    ip_protocol = -1
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_8080_from_subnet" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv4 = aws_vpc.tf_vpc.cidr_block
-    from_port = -1
-    ip_protocol = "tcp"
+    from_port = 8080
+    ip_protocol = -1
     to_port = 8080
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_443_from_ipv6_internet" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv6 = "::/0"
-    from_port = -1
+    from_port = 443
     ip_protocol = "tcp"
     to_port = 443
 }
