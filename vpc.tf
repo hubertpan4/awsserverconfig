e771a5ae305bd8188169e917b9dc5b9b56093275
hp <hp@example.com>
2025-05-27 22:55:38 -0400

    assign ipv6 cidr blocks to subnets

--- vpc.tf
+++ vpc.tf
@@ -15,6 +15,7 @@ resource "aws_internet_gateway" "tf_vpc_gw" {
 resource "aws_subnet" "subnet_a" {
     vpc_id = aws_vpc.tf_vpc.id 
     cidr_block= "192.168.1.0/24"
+    ipv6_cidr_block = cidrsubnet(aws_vpc.tf_vpc.ipv6_cidr_block,8,1) 
     availability_zone="us-east-2a"
     assign_ipv6_address_on_creation=true
 }
@@ -22,6 +23,7 @@ resource "aws_subnet" "subnet_a" {
 resource "aws_subnet" "subnet_b" {
     vpc_id = aws_vpc.tf_vpc.id 
     cidr_block= "192.168.2.0/24"
+    ipv6_cidr_block = cidrsubnet(aws_vpc.tf_vpc.ipv6_cidr_block,8,2) 
     availability_zone="us-east-2b"
 }
 
