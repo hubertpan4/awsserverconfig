76436d7dd9bde9139679d520cd6f6c23cf5c62ec
hp <hp@example.com>
2025-05-27 22:24:36 -0400

    fix bug

--- server-security-group.tf
+++ server-security-group.tf
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_server_egress_ipv6" {
 resource "aws_vpc_security_group_ingress_rule" "allow_server_tcp_8080_from_subnet" {
     security_group_id = aws_security_group.server.id
     cidr_ipv4 = aws_vpc.tf_vpc.cidr_block
-    from_port = -1
+    from_port = 8080
     ip_protocol = "tcp"
     to_port = 8080
 }
