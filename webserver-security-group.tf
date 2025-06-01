09ec961c356939ecbbc89e76d20c8fb6eadf7afe
Hubert Pan <hubert.pan3@gmail.com>
2025-06-01 15:18:32 -0400

    comment out allow_tcp_8080_from_subnet rule

--- webserver-security-group.tf
+++ webserver-security-group.tf
@@ -16,6 +16,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_egress_ipv6" {
     ip_protocol = -1
 }
 
+/*
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_8080_from_subnet" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv4 = aws_vpc.tf_vpc.cidr_block
@@ -23,6 +24,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp_8080_from_subnet" {
     ip_protocol = "tcp"
     to_port = 8080
 }
+*/
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tcp_443_from_ipv6_internet" {
     security_group_id = aws_security_group.webserver.id
