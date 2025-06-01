066027dc0d3f270d1c0669b6c8805b6121c76035
Hubert Pan <hubert.pan3@gmail.com>
2025-06-01 15:36:07 -0400

    try to enable ssh access for webserver security group again.

--- webserver-security-group.tf
+++ webserver-security-group.tf
@@ -38,5 +38,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_ipv6_internet" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv6 = "::/0"
     from_port = 22
+    ip_protocol = "tcp"
     to_port = 22
 }
