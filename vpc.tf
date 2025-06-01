7d052f8e84332a6183422f8846665b039720b596
Hubert Pan <hubert.pan3@gmail.com>
2025-06-01 14:58:33 -0400

    add name tags to vpc components

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,9 @@ resource "aws_vpc" "tf_vpc" {
 
 resource "aws_internet_gateway" "tf_vpc_gw" {
     vpc_id = aws_vpc.tf_vpc.id
-
+    tags = {
+        Name = "tf_igw"
+    }
 }
 
 resource "aws_subnet" "subnet_a" {
@@ -18,6 +20,9 @@ resource "aws_subnet" "subnet_a" {
     ipv6_cidr_block = cidrsubnet(aws_vpc.tf_vpc.ipv6_cidr_block,8,1) 
     availability_zone="us-east-2a"
     assign_ipv6_address_on_creation=true
+    tags = {
+        Name = "tf_subnet_a"
+    }
 }
 
 resource "aws_subnet" "subnet_b" {
@@ -26,6 +31,9 @@ resource "aws_subnet" "subnet_b" {
     ipv6_cidr_block = cidrsubnet(aws_vpc.tf_vpc.ipv6_cidr_block,8,2) 
     availability_zone="us-east-2b"
     assign_ipv6_address_on_creation=true
+    tags = {
+        Name = "tf_subnet_b"
+    }
 }
 
 resource "aws_route_table" "tf_rt_tbl" {
@@ -39,26 +47,41 @@ resource "aws_route" "internet_ip4" {
     route_table_id =  aws_route_table.tf_rt_tbl.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.tf_vpc_gw.id
+    tags = {
+        Name = "internet_ip4"
+    }
 }
 
 resource "aws_route" "internet_ip6" {
     route_table_id =  aws_route_table.tf_rt_tbl.id
     destination_ipv6_cidr_block = "::/0"
     gateway_id = aws_internet_gateway.tf_vpc_gw.id
+    tags = {
+        Name = "internet_ip6"
+    }
 }
 
 resource "aws_vpc_endpoint" "s3" {
     vpc_id = aws_vpc.tf_vpc.id
     vpc_endpoint_type="Gateway"
     service_name="com.amazonaws.us-east-2.s3"
+    tags = {
+        Name = "s3_gateway"
+    }
 }
 
 resource "aws_route_table_association" "subnet_a_tf_rt_tbl" {
     subnet_id = aws_subnet.subnet_a.id
     route_table_id = aws_route_table.tf_rt_tbl.id
+    tags = {
+        Name = "subnet_a_tf_rt_tbl_assoc"
+    }
 }
 
 resource "aws_route_table_association" "subnet_b_tf_rt_tbl" {
     subnet_id = aws_subnet.subnet_b.id
     route_table_id = aws_route_table.tf_rt_tbl.id
+    tags = {
+        Name = "subnet_b_tf_rt_tbl_assoc"
+    }
 }
