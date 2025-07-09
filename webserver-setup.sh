4ec12bc40e22d06f2f02ee0b73eccf12f69fd708
Hubert Pan <hubert.pan3@gmail.com>
2025-07-08 23:08:20 -0400

    fix issue where java failed to install

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo yum update
 sudo yum upgrade install -y
-sudo yum install java
+sudo yum install java -y
+sudo yum install -y aws-cli
 echo "Attempting to list S3 bucket: com.icarusfrog.test" >> /var/log/cloud-init-output.log
 aws s3 ls s3://com.icarusfrog.test/ >> /var/log/cloud-init-output.log 2>&1
 # wget https://github.com/hubertpan3/aws-demo/releases/download/0.0.1/aws-demo-0.0.1-SNAPSHOT.jar
