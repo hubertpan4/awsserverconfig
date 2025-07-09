9250c743f4e90a902d344c48d79bb17eb43f607b
Hubert Pan <hubert.pan3@gmail.com>
2025-07-08 23:09:13 -0400

    add webserver instance profile

--- webserver-role.tf
+++ webserver-role.tf
@@ -25,3 +25,12 @@ resource "aws_iam_role_policy_attachment" "attach_writeMetrics" {
   role       = aws_iam_role.webserver_role.name
   policy_arn = aws_iam_policy.writeMetrics.arn
 }
+
+resource "aws_iam_instance_profile" "webserver_instance_profile" {
+  name = "webserver-instance-profile"
+  role = aws_iam_role.webserver_role.name
+
+  tags = {
+    Name = "webserver_role_profile"
+  }
+}
