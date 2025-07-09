264e3099d064ddb7fbe6f6e08afe46f647e901d5
Hubert Pan <hubert.pan3@gmail.com>
2025-07-08 22:48:28 -0400

    update setup instructions

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -2,6 +2,8 @@
 sudo yum update
 sudo yum upgrade install -y
 sudo yum install java
+echo "Attempting to list S3 bucket: com.icarusfrog.test" >> /var/log/cloud-init-output.log
+aws s3 ls s3://com.icarusfrog.test/ >> /var/log/cloud-init-output.log 2>&1
 # wget https://github.com/hubertpan3/aws-demo/releases/download/0.0.1/aws-demo-0.0.1-SNAPSHOT.jar
 # java -jar aws-demo-0.0.1-SNAPSHOT.jar
 # sudo yum install nginx
