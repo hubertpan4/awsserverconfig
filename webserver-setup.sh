f5708e5630edc6ce5482cad23503c6f2a3e5e3bb
Hubert Pan <hubert.pan3@gmail.com>
2025-07-08 21:39:53 -0400

    update webserver setup script to include jar pull and startup command

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo yum update
 sudo yum upgrade install -y
-wget https://github.com/hubertpan3/aws-demo/releases/download/0.0.1/aws-demo-0.0.1-SNAPSHOT.jar
-java -jar aws-demo-0.0.1-SNAPSHOT.jar
+sudo yum install java
+# wget https://github.com/hubertpan3/aws-demo/releases/download/0.0.1/aws-demo-0.0.1-SNAPSHOT.jar
+# java -jar aws-demo-0.0.1-SNAPSHOT.jar
 # sudo yum install nginx
 # sudo systemctl start nginx
