cd65c00bdc3d524ffa1e30010faee6fe6d17e968
Hubert Pan <hubert.pan3@gmail.com>
2025-07-13 08:29:31 -0400

    update start up script to capture logs

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -6,7 +6,8 @@ sudo yum install -y aws-cli
 echo "Attempting to list S3 bucket: com.icarusfrog.us.east.2.code.bucket" >> /var/log/cloud-init-output.log
 #aws s3 ls s3://com.icarusfrog.us.east.2.code.bucket/ >> /var/log/cloud-init-output.log 2>&1
 aws s3 cp s3://com.icarusfrog.us.east.2.code.bucket/aws-demo-0.0.1-SNAPSHOT.jar bob.jar --debug --endpoint-url http://s3.dualstack.us-east-2.amazonaws.com/
-java -jar bob.jar -Dspring.profiles.active=aws
+touch bob.txt
+java -jar bob.jar -Dspring.profiles.active=aws >> bob.txt
 # aws s3 cp s3://com.icarusfrog.us.east.2.code.bucket/aws-demo-0.0.1-SNAPSHOT.jar bob.jar
 # wget https://github.com/hubertpan3/aws-demo/releases/download/0.0.1/aws-demo-0.0.1-SNAPSHOT.jar
 # aws s3 cp s3://com.icarusfrog.us.east.2.code.bucket/aws-demo-0.0.1-SNAPSHOT.jar bob.jar --debug --endpoint-url http://s3.dualstack.us-east-2.amazonaws.com/
