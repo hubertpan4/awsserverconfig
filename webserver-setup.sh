1f5dca155ae996224b9f29e7b19752163451668f
Hubert Pan <hubert.pan3@gmail.com>
2025-07-14 07:04:07 -0400

    add SPRING_PROFILES_ACTIVE env variable to enable aws and debug profiles

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -6,6 +6,7 @@ sudo yum install -y aws-cli
 echo "Attempting to list S3 bucket: com.icarusfrog.us.east.2.code.bucket" >> /var/log/cloud-init-output.log
 #aws s3 ls s3://com.icarusfrog.us.east.2.code.bucket/ >> /var/log/cloud-init-output.log 2>&1
 aws s3 cp s3://com.icarusfrog.us.east.2.code.bucket/aws-demo-0.0.1-SNAPSHOT.jar bob.jar --debug --endpoint-url http://s3.dualstack.us-east-2.amazonaws.com/
+export SPRING_PROFILES_ACTIVE=aws,debug
 touch bob.txt
 java -jar bob.jar -Dspring.profiles.active=aws >> bob.txt
 # aws s3 cp s3://com.icarusfrog.us.east.2.code.bucket/aws-demo-0.0.1-SNAPSHOT.jar bob.jar
