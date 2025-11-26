79fa9700107afe3dfb1100857ab85c5dcc6e7c75
hubert <hubert.pan3@gmail.com>
2025-11-25 22:48:13 -0500

    open up port 8000 on web server ec2 instances in preparation for hosting fastapi based endpoints.

--- webserver-security-group.tf
+++ webserver-security-group.tf
@@ -50,6 +50,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tcp_443_from_ipv6_internet
     to_port = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_8000_from_ipv6_internet" {
+    security_group_id = aws_security_group.webserver.id
+    cidr_ipv6 = "::/0"
+    from_port = 8000
+    ip_protocol = "tcp"
+    to_port = 8000
+}
+
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_ipv6_internet" {
     security_group_id = aws_security_group.webserver.id
     cidr_ipv6 = "::/0"
